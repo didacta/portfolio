ae54731f53c54feb8db9d53cc5789c9cb2c6b0d5
Igor Yermak <igor.yermak@gmail.com>
2018-08-14 01:30:47 -0400

    added css changes

---
+++ code/test.js
@@ -0,0 +1,13 @@
+import React, { Fragment } from 'react';
+import PropTypes from 'prop-types';
+
+/**
+ * A test component
+ */
+const Test = ({ _body }) => (
+  <Fragment>
+    { _body }
+  </Fragment>
+);
+
+export default Test;
